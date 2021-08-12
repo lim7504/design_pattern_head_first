a608797235737241bbf206ab0fe41cf92130f816
jslim <jslim@lifegoeson.kr>
2021-08-12 10:26:35 +0900

    refactoring

--- src/strategy/Main.java
+++ src/strategy/Main.java
@@ -1,6 +1,6 @@
 package strategy;
 
-public class MiniDuckSimulator {
+public class Main {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
         mallard.performFly();
